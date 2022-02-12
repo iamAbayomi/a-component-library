91f7194e6cf770c74285fcdf91b5dd18e7caca1b
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-02-12 15:00:21 +0100

    feature: added design system/ checklist

--- src/components/Checkbox/Checkbox.stories.tsx
+++ src/components/Checkbox/Checkbox.stories.tsx
@@ -1,1 +1,17 @@
-export {}
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import React from "react";
+import Checkbox from "./Checkbox";
+
+
+export default {
+    title: 'Design System/Checkbox',
+    component: Checkbox
+} as ComponentMeta<typeof Checkbox>
+
+
+// const Primary: ComponentStory<typeof Checkbox> = () => <Checkbox />
+
+const Template: ComponentStory<typeof Checkbox> = (args) => <Checkbox {...args} />
+
+export const Primary = Template.bind({})
+Primary.args = {}
