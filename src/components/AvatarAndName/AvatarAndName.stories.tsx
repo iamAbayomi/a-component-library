2581a945558a11668cf6f6a7a9913e76eb7fea34
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-02-12 14:02:23 +0100

    refactor: changed directory of stories

--- src/components/AvatarAndName/AvatarAndName.stories.tsx
+++ src/components/AvatarAndName/AvatarAndName.stories.tsx
@@ -1,14 +1,12 @@
-// import { ComponentMeta, ComponentStory } from "@storybook/react";
-// import AvatarandName from "./AvatarAndName";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import AvatarandName from "./AvatarAndName";
 
-// export default {
-//     title: 'AvatarAndName',
-//     component: AvatarandName
-// } as ComponentMeta<typeof AvatarandName>
+export default {
+    title: 'AvatarAndName',
+    component: AvatarandName
+} as ComponentMeta<typeof AvatarandName>
 
-// const Template: ComponentStory<typeof AvatarandName> = (args) => <AvatarandName {...args} />
+const Template: ComponentStory<typeof AvatarandName> = (args) => <AvatarandName {...args} />
 
-// export const Primary = Template.bind({})
-// Primary.args = {name: 'James Brown', imageSrc: 'https://avatars2.githubusercontent.com/u/132554'}
-
-export {}
+export const Primary = Template.bind({})
+Primary.args = {name: 'James Brown', imageSrc: 'https://avatars2.githubusercontent.com/u/132554'}
--- src/components/Badge/Badge.stories.tsx
+++ src/components/Badge/Badge.stories.tsx
@@ -1,34 +1,33 @@
-// import { ComponentMeta, ComponentStory } from "@storybook/react";
-// import Badge from "./Badge";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import Badge from "./Badge";
 
 
-// export default {
-//     title: 'Badge',
-//     component: Badge
-// } as ComponentMeta <typeof Badge>
+export default {
+    title: 'Badge',
+    component: Badge
+} as ComponentMeta <typeof Badge>
 
 
-// const Template: ComponentStory<typeof Badge> = (args) => <Badge {...args} />
+const Template: ComponentStory<typeof Badge> = (args) => <Badge {...args} />
 
-// export const active = Template.bind({})
-// active.args = {BadgeText: 'active'}
+export const active = Template.bind({})
+active.args = {BadgeText: 'active'}
 
-// export const block: ComponentStory<typeof Badge> = () => <Badge BadgeText={'block'} />
+export const block: ComponentStory<typeof Badge> = () => <Badge BadgeText={'block'} />
 
-// export const successful: ComponentStory<typeof Badge> = () => <Badge BadgeText={'successful'} />
+export const successful: ComponentStory<typeof Badge> = () => <Badge BadgeText={'successful'} />
 
-// export const cancelled: ComponentStory<typeof Badge> = () => <Badge BadgeText={'cancelled'} />
+export const cancelled: ComponentStory<typeof Badge> = () => <Badge BadgeText={'cancelled'} />
 
-// export const pending: ComponentStory<typeof Badge> = () => <Badge BadgeText={'pending'} />
+export const pending: ComponentStory<typeof Badge> = () => <Badge BadgeText={'pending'} />
 
-// export const processing: ComponentStory<typeof Badge> = () => <Badge BadgeText={'processing'} />
+export const processing: ComponentStory<typeof Badge> = () => <Badge BadgeText={'processing'} />
 
-// export const decline: ComponentStory<typeof Badge> = () => <Badge BadgeText={'decline'} />
+export const decline: ComponentStory<typeof Badge> = () => <Badge BadgeText={'decline'} />
 
-// export const failed: ComponentStory<typeof Badge> = () => <Badge BadgeText={'failed'} />
+export const failed: ComponentStory<typeof Badge> = () => <Badge BadgeText={'failed'} />
 
-// export const inProgess: ComponentStory<typeof Badge> = () => <Badge BadgeText={'in progress'} />
+export const inProgess: ComponentStory<typeof Badge> = () => <Badge BadgeText={'in progress'} />
 
-// export const completed: ComponentStory<typeof Badge> = () => <Badge BadgeText={'completed'} />
+export const completed: ComponentStory<typeof Badge> = () => <Badge BadgeText={'completed'} />
 
-export {}
