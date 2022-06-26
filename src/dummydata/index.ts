bfc5ddb3395e6fb19176c9d36e9542a56b127fea
iamAbayomi <oladinifolawiyo>
2022-06-26 20:29:17 +0100

    feat: add navigation dummy data

---
+++ src/dummydata/index.ts
@@ -0,0 +1,32 @@
+
+export const navigationMenuExample = [
+    {
+        imageSrc: "",
+        menuText: "Home"
+    },
+    {
+        imageSrc: "",
+        menuText: "Orders"
+    },
+    {
+        imageSrc: "",
+        menuText: "Products"
+    },
+    {
+        imageSrc: "",
+        menuText: "Customers"
+    },
+    {
+        imageSrc: "",
+        menuText: "Analytics"
+    },
+    {
+        imageSrc: "",
+        menuText: "Marketing"
+    },
+    {
+        imageSrc: "",
+        menuText: "Discounts"
+    },
+
+]
