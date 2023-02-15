3836c383be747d52ad37544f7602aaf9ce650e76
iamAbayomi <oladinifolawiyo@gmail.com>
2023-02-15 13:29:05 +0100

    fix: add width to image element to fix the storybook publishing error

--- src/components/MediaCard/MediaCard.tsx
+++ src/components/MediaCard/MediaCard.tsx
@@ -1,30 +1,35 @@
-import React from "react"
-import { Box,  Image, Text } from "@chakra-ui/react"
-import Button from "../Button/Button"
+import React from "react";
+import { Box, Image, Text } from "@chakra-ui/react";
+import Button from "../Button/Button";
 
 type props = {
-    imageSrc: string,
-    title: string,
-    subTitle: string,
-    buttonText: string
-}
+  imageSrc: string;
+  title: string;
+  subTitle: string;
+  buttonText: string;
+};
 
-const MediaCard = ({imageSrc, title, subTitle, buttonText}: props) => {
-    return (
-       <Box maxWidth={"400px"} boxShadow={"0 1px 4px 0 rgba(0,0,0,0.2)"}
-            padding={"20px"}>
-            <Image src={imageSrc} />
-            <Box display={"flex"}>  
-                <Text>{title}</Text>
-            </Box>
-            <Text>{subTitle}</Text>
-            <Button 
-                height="40px" backgroundColor="blue"
-                buttonText="Learn More"  width="200px"
-                color="white"
-            />
-       </Box>
-    )
-}
+const MediaCard = ({ imageSrc, title, subTitle, buttonText }: props) => {
+  return (
+    <Box
+      maxWidth={"400px"}
+      boxShadow={"0 1px 4px 0 rgba(0,0,0,0.2)"}
+      padding={"20px"}
+    >
+      <Image src={imageSrc} width={"100%"} />
+      <Box display={"flex"}>
+        <Text>{title}</Text>
+      </Box>
+      <Text>{subTitle}</Text>
+      <Button
+        height="40px"
+        backgroundColor="blue"
+        buttonText="Learn More"
+        width="200px"
+        color="white"
+      />
+    </Box>
+  );
+};
 
-export default MediaCard
+export default MediaCard;
