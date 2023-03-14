a6754c819b29a74d7918f10f0719ebc012cb3ee7
iamAbayomi <oladinifolawiyo@gmail.com>
2023-03-14 08:28:05 +0100

    Feat: add docs to components

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,30 +1,33 @@
-import styled from "styled-components"
-import React from "react"
+import styled from "styled-components";
+import React, { FC } from "react";
+
+/**
+ * @typedef props
+ *  The URL of the Avatar's image.
+ * @prop {string} imageSrc
+ */
 
 /**
 - Use an avatar for attributing actions or content to specific users.
 - The user's name should always be present when using Avatar – either printed beside the avatar or in a tooltip.
-    @method
 **/
 
-export interface props {
-    imageSrc: string
-}
+const Avatar: FC<props> = (props) => {
+  return (
+    <div>
+      <AvatarImage src={props.imageSrc} />
+    </div>
+  );
+};
 
-const Avatar: React.FC<props> = (props) =>{
-   return(
-        <div>
-            <AvatarImage src={props.imageSrc}  />
-        </div>
-    ) 
-}
-
-export default Avatar
-
-
+export default Avatar;
 
 const AvatarImage = styled.img`
-    margin: 0px 20px 0px 0px;
-    height: 40px;
-    border-radius: 50%;
-`
+  margin: 0px 20px 0px 0px;
+  height: 40px;
+  border-radius: 50%;
+`;
+
+export type props = {
+  imageSrc: string;
+};
