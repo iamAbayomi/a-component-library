c7e260e474f3fe883853f2e2f0279491624fc165
iamAbayomi <oladinifolawiyo@gmail.com>
2023-04-11 14:47:45 +0100

    feat: add avatar documentation

--- src/components/Avatar/Avatar.stories.tsx
+++ src/components/Avatar/Avatar.stories.tsx
@@ -1,6 +1,6 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 import React from "react";
-import Avatar from "./Avatar";
+import Avatar from ".";
 
 export default {
   title: "Design System/Avatar",
