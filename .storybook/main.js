8d36b861743744d3260c34c951fcab7a72b1cb29
iamAbayomi <oladinifolawiyo@gmail.com>
2023-04-13 09:15:22 +0100

    feat: fix storybook issue

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,25 +1,23 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx|mdx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-docs",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    '@storybook/preset-create-react-app',
-    '@storybook/addon-a11y',
-  ],
+  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx|mdx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-docs", "@storybook/addon-essentials",
+    "@storybook/addon-interactions", '@storybook/preset-create-react-app', '@storybook/addon-a11y', "@storybook/addon-mdx-gfm"],
   "staticDirs": ['../public'],
-  "framework": "@storybook/react",
-  "features": { emotionAlias: false },
+  "framework": {
+    name: "@storybook/react-webpack5",
+    options: {}
+  },
+  "features": {
+    emotionAlias: false
+  },
   "refs": {
     '@chakra-ui/react': {
-      disable: true,
-    },
+      disable: true
+    }
   },
-  "webpackFinal": async (config, { configType }) => {
+  "webpackFinal": async (config, {
+    configType
+  }) => {
     // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
     // You can change the configuration based on that.
     // 'PRODUCTION' is used when building the static version of storybook.
@@ -30,9 +28,12 @@ module.exports = {
     config.module.rules.push({
       test: /\.mjs$/,
       include: /node_modules/,
-      type: 'javascript/auto',
+      type: 'javascript/auto'
     });
     // Return the altered config
     return config;
+  },
+  docs: {
+    autodocs: true
   }
-}
+};
--- rollup.config.js
+++ rollup.config.js
@@ -5,8 +5,26 @@ import dts from "rollup-plugin-dts";
 import postcss from 'rollup-plugin-postcss'
 import { terser } from "rollup-plugin-terser";
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import json from "rollup-plugin-json"
 
 const packageJson = require("./package.json");
+
+const pkg = process.env.LERNA_PACKAGE_NAME &&
+    require(`${process.env.LERNA_PACKAGE_NAME}/package.json`);
+
+const dependencies = ({ dependencies }) => Object.keys(dependencies || {});
+
+const pkgdependencies = dependencies(pkg);
+
+/* exported rollup configuration */
+const config = {
+    /* your config goes here... */
+    /* id is the source name if list of dependencies includes
+     * id source name, then mark it as external,
+     */
+    external: id => pkgdependencies.includes(id)
+};
+
 
 export default [
     {
@@ -37,6 +55,7 @@ export default [
         input: "dist/esm/types/index.d.ts",
         output: [{ file: "dist/index.d.ts", format: "esm" }],
         plugins: [dts()],
-        external: [/\.css$/],  
+        external: [/\.css$/],
     },
+    config
 ];
